818c96fab50430ab3176df5bcd317e33151d1a2f
45657 <456573862z@gmail.com>
2025-10-28 13:49:34 +0300

    text preparing base class

--- chromanew/text_utils.py
+++ chromanew/text_utils.py
@@ -1,14 +1,30 @@
+from typing import Dict, Any, List
+
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
+MAX_CHUNK_SIZE = 512
+CHUNK_OVERLAP = 64
 
-def split_text_into_chunks(text: str, metadata: Dict[str, Any]) -> List[Any]:
-    """Разделение текста на чанки с сохранением метаданных."""
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=settings.MAX_CHUNK_SIZE,
-        chunk_overlap=settings.CHUNK_OVERLAP,
-        length_function=len,
-        is_separator_regex=False,
-    )
 
-    chunks = text_splitter.create_documents(texts=[text], metadatas=[metadata])
-    return chunks
+class TextPreProcessor:
+    @staticmethod
+    def split_text_into_chunks(text: str, metadata: Dict[str, Any]) -> List[Any]:
+        """Разделение текста на чанки с сохранением метаданных."""
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=MAX_CHUNK_SIZE,
+            chunk_overlap=CHUNK_OVERLAP,
+            length_function=len,
+            is_separator_regex=False,
+        )
+        chunks = text_splitter.create_documents(texts=[text], metadatas=[metadata])
+        return chunks
+
+    def chanks_by_summary(self, text: str, metadata: Dict[str, Any], summary_size: int = MAX_CHUNK_SIZE) -> List[Any]:
+        """
+        1 Chank by 1 input text
+        Use an AI summarization
+        """
+        # integrate AI API
+        pass
+
+
